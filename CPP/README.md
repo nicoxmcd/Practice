ad6877210cf7b30413d9c258063e22c9b179209b
Nicole X McDermott <35404943+nicoxmcd@users.noreply.github.com>
2025-06-23 21:33:34 -0400

    Accidentally Duplicated Java files

--- CPP/README.md
+++
@@ -1,2 +0,0 @@
-# AP Computer Science in JAVA
-> 2019-2020 school year
--- CPP/Random.java
+++
@@ -1,9 +0,0 @@
-import java.lang.Math;
-
-// class Main {
-class Lesson_9_Activity_Three {
-    public static void main(String[] args) {
-      
-    System.out.println((int)(Math.random()*21) + 20);
-    }
-}
