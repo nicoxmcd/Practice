0f61b4589d5a503cbb5e23f2bff9592b7d0a485e
Nicole X McDermott <35404943+nicoxmcd@users.noreply.github.com>
2025-07-14 21:33:45 -0400

    Create functions.py

---
+++ Python/functions.py
@@ -0,0 +1,24 @@
+def directions_to_timesSq():
+  print("Walk 4 mins to 34th St Herald Square train station.")
+  print("Take the Northbound N, Q, R, or W train 1 stop.")
+  print("Get off the Times Square 42nd Street stop.")
+  print("Take lots of pictures!")
+
+# Call your function here:
+directions_to_timesSq()
+
+print("Checking the weather for you!")
+
+def weather_check():
+  print("Looks great outside! Enjoy your trip.")
+  print("False Alarm, the weather changed! There is a thunderstorm approaching. Cancel your plans and stay inside.")
+
+weather_check()
+
+
+#  Using parameters
+def generate_trip_instructions(location):
+  print("Looks like you are planning a trip to visit " + location)
+  print("You can use the public subway system to get to " + location)
+
+generate_trip_instructions("Grand Central Station")
