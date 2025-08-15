d23557fb3f64a1a35f064ec7c4253fa8ae62a933
Nicole X McDermott <35404943+nicoxmcd@users.noreply.github.com>
2025-08-14 21:56:57 -0400

    Create greetingLoop.py

---
+++ Python/greetingLoop.py
@@ -0,0 +1,4 @@
+greeting = ["hello","hola","hallo","안녕하세요","hi","good morning","buenos días","guten morgen","how are you?","¿cómo estás?","wie ghet's?","어떻게 지내세요?"]
+
+for word in greeting:
+    print(word)
